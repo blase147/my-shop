a5324bfcb0fcc7a65fb74043d3de38d4a204ef5b
blase147 <solarmails2@gmail.com>
2023-12-21 00:34:11 +0100

    update product details

--- src/components/copyright/creditCardLogos.jsx
+++ src/components/copyright/creditCardLogos.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const CreditCardLogos = () => (
   <div className="creditCardLogos">
-    <img src="../../images/visa.png" alt="Visa Logo" width="30" height="20" className="credite_card" />
-    <img src="../../images/mastercard.png" alt="Mastercard Logo" width="30" height="20" className="credite_card" />
-    <img src="../../images/paypal.png" alt="American Express Logo" width="30" height="20" className="credite_card" />
+    <img src="../../images/visa.png" alt="Visa Logo" width="30" height="20" className="credit_card" />
+    <img src="../../images/mastercard.png" alt="Mastercard Logo" width="30" height="20" className="credit_card" />
+    <img src="../../images/paypal.png" alt="American Express Logo" width="30" height="20" className="credit_card" />
   </div>
 );
 
--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-// import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faShareAlt, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 import { fetchCarById } from '../../Redux/Reducers/productSlice';
 import {
   addToCart, decrementQuantity, incrementQuantity,
@@ -14,12 +15,14 @@ import Search from '../search/search';
 import Newsletter from '../newsletterSub/newsletterSub';
 import Footer from '../footer/footer';
 import CopyRight from '../copyright/copyright';
+import CreditCardLogos from '../copyright/creditCardLogos';
+import Services from '../services/services';
 
 const Product = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const product = useSelector((state) => state.product.product);
-  // const quantity = useSelector((state) => state.cart.quantity);
+  const [activeItem, setActiveItem] = useState('description'); // Set initial state to 'description'
   const cart = useSelector((state) => state.cart.cart);
 
   useEffect(() => {
@@ -45,47 +48,131 @@ const Product = () => {
       </div>
       <Menu />
       {product ? (
-        <div className="product">
-          <img className="product__image" src={product.product_image_url} alt={product.name} />
-          <div className="product__info">
-            <p className="product__title">{product.name}</p>
-            <p className="product__price">
-              <small>$</small>
-              <strong>{product.price}</strong>
-            </p>
-            <p className="product__desc">{product.description}</p>
-            <div className="myCart_addmorebtn">
-              <diV className="dec_inc_cont">
+        <>
+          <div className="product">
+            <img
+              className="product__image"
+              src={product.product_image_url}
+              alt={product.name}
+            />
+            <div className="product__info">
+              <FontAwesomeIcon className="heart_icon" icon={faHeart} />
+              <p className="product__title">{product.name}</p>
+              <p className="product__price">
+                <small>$</small>
+                <strong>{product.price}</strong>
+              </p>
+              <p className="product__desc">{product.description}</p>
+              <div className="myCart_addmorebtn">
+                <div className="dec_inc_cont">
+                  <button
+                    type="button"
+                    onClick={() => dispatch(decrementQuantity(product))}
+                  >
+                    -
+                  </button>
+                  <strong>{getTotalQuantity()}</strong>
+                  <button
+                    type="button"
+                    onClick={() => dispatch(incrementQuantity(product))}
+                  >
+                    +
+                  </button>
+                </div>
                 <button
+                  className="add_to_cart"
                   type="button"
-                  onClick={() => dispatch(decrementQuantity(product))}
+                  onClick={() => dispatch(addToCart(product))}
                 >
-                  -
+                  Add to Cart
                 </button>
-                <strong>{getTotalQuantity()}</strong>
-                <button
-                  type="button"
-                  onClick={() => dispatch(incrementQuantity(product))}
-                >
-                  +
-                </button>
-              </diV>
-              <button
-                className="add_to_cart"
-                type="button"
-                onClick={() => dispatch(addToCart(product))}
-              >
-                Add to Cart
+              </div>
+              <button className="paypal_btn" type="button">
+                Buy with Paypal
               </button>
+              <div className="share_question_cont">
+                <div className="share_question">
+                  <FontAwesomeIcon className="share_icon" icon={faShareAlt} />
+                  <p>Share</p>
+                </div>
+                <div className="share_question">
+                  <FontAwesomeIcon
+                    className="question_icon"
+                    icon={faQuestionCircle}
+                  />
+                  <p>Ask a Question</p>
+                </div>
+              </div>
+              <hr />
+              <p>GUARANTEED SAFE CHECKOUT</p>
+              <CreditCardLogos />
+              <p>Order ships within 5 to 10 days.</p>
             </div>
-            <button className="paypal_btn" type="button">
-              Buy with Paypal
-            </button>
           </div>
-        </div>
+          <div className="product_desc_delivery_cont">
+            <div
+              className="tab"
+              role="button"
+              tabIndex={0}
+              onClick={() => setActiveItem('description')}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  setActiveItem('description');
+                }
+              }}
+            >
+              DESCRIPTION
+            </div>
+            <div
+              className="tab"
+              role="button"
+              tabIndex={0}
+              onClick={() => setActiveItem('delivery')}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  setActiveItem('delivery');
+                }
+              }}
+            >
+              DELIVERY POLICY
+            </div>
+            <div
+              className="tab"
+              role="button"
+              tabIndex={0}
+              onClick={() => setActiveItem('shipping')}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  setActiveItem('shipping');
+                }
+              }}
+            >
+              SHIPPING & RETURN
+            </div>
+          </div>
+          <div className="product_desc_delivery_text">
+            {activeItem === 'description' && (
+              <div className="text">
+                <p>{product.description}</p>
+              </div>
+            )}
+            {activeItem === 'delivery' && (
+              <div className="text">
+                <p>Delivery text </p>
+              </div>
+            )}
+            {activeItem === 'shipping' && (
+              <div className="text">
+                <p>Shipping text</p>
+              </div>
+            )}
+          </div>
+        </>
       ) : (
         <div>Loading...</div>
       )}
+      <hr />
+      <Services />
       <Link to="/products">Go to Products</Link>
       <Link to="/viewCart">Go to Cart</Link>
       <Link to="/">Go back</Link>
@@ -95,13 +182,5 @@ const Product = () => {
     </section>
   );
 };
-
-// Product.propTypes = {
-//   id: PropTypes.number.isRequired,
-//   name: PropTypes.string.isRequired,
-//   price: PropTypes.number.isRequired,
-//   image: PropTypes.string.isRequired,
-//   desc: PropTypes.string.isRequired,
-// };
 
 export default Product;
